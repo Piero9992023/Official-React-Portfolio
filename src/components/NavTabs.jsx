5c2b098a924a59423610bbdb86488f4cb4c50c1b
Piero Rodriguez <piero@Pieros-MacBook-Pro.local>
2023-12-13 15:06:45 -0500

    made navbar responsive for smaller devices, added dropdown menu for the user, added bold to navbar links

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,36 +1,90 @@
+import React, { useEffect , useState } from 'react'
 import { Link } from 'react-scroll'
 import '../index.css'
 
 const NavTabs = () => {
-
-
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsMenuOpen((prev) => !prev);
+    };
+  
+    // Close the menu when the screen size becomes larger
+    useEffect(() => {
+      const handleResize = () => {
+        if (window.innerWidth >= 768) {
+          setIsMenuOpen(false);
+        }
+      };
+  
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+  
     return (
-        <nav className= 'bg-white dark:bg-gray-950 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600'>
-            <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-                <h2 className='text-white text-xl'>Piero Rodriguez</h2>
-            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-            <ul className= "flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li className='cursor-pointer'>
-                    <Link className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' aria-current="page" to= "home" spy={true} smooth={true} offset={-100} duration={500} >Home</Link>
-                </li>
-                <li className='cursor-pointer'>
-                    <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "about" spy={true} smooth={true} offset={-100} duration={500} >About Me</Link>
-                </li>
-                <li className='cursor-pointer'>
-                    <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "projects" spy={true} smooth={true} offset={-100} duration={500}>Projects</Link>
-                </li>
-                <li className='cursor-pointer'>
-                    <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "resume" spy={true} smooth={true} offset={-100} duration={500} >Resume</Link>
-                </li>
-                <li  className='cursor-pointer'>
-                    <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "contact" spy={true} smooth={true} offset={-150} duration={500}>Contact Me</Link>
-                </li>
+      <nav className="bg-white dark:bg-gray-950 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl mx-auto p-4 flex items-center justify-between">
+          <h2 className="text-white text-2xl font-bold">Piero Rodriguez</h2>
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-gray-500 hover:text-gray-700 focus:outline-none"
+            >
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </button>
+          </div>
+          <ul
+            className={`${
+              isMenuOpen ? 'block' : 'hidden'
+            } mt-5 mr-5 font-bold space-y-2 md:space-y-0 md:space-x-8 md:flex md:w-auto md:absolute md:top-0 md:right-0`}
+          >
+            <li className="cursor-pointer">
+              <Link
+                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                aria-current="page"
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                Home
+              </Link>
+            </li>
+            <li className='cursor-pointer'>
+                <Link onClick={() => setIsMenuOpen(false)} className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "about" spy={true} smooth={true} offset={-100} duration={500} >About Me</Link>
+            </li>
+            <li className='cursor-pointer'>
+                <Link onClick={() => setIsMenuOpen(false)} className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "projects" spy={true} smooth={true} offset={-100} duration={500}>Projects</Link>
+            </li>
+            <li className='cursor-pointer'>
+                <Link onClick={() => setIsMenuOpen(false)} className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "resume" spy={true} smooth={true} offset={-100} duration={500} >Resume</Link>
+            </li>
+            <li  className='cursor-pointer'>
+                <Link onClick={() => setIsMenuOpen(false)} className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "contact" spy={true} smooth={true} offset={-150} duration={500}>Contact Me</Link>
+            </li>
             </ul>
-            </div>
-            </div>
-        </nav>
-    )
-}
+      </div>
+    </nav>
+  );
+};
 
 
 export default NavTabs
