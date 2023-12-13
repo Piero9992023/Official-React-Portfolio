79c5c92f65b5b3b4df5251c144fe5f3e8ad0700c
Piero Rodriguez <piero@Pieros-MacBook-Pro.local>
2023-12-13 12:16:18 -0500

    installed additonal tailwind packages, removed unnecessary code and completed tailwind css in Navtabs

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
 import { Link } from 'react-scroll'
 import '../index.css'
 
 const NavTabs = () => {
 
-    const [click, setClick] = useState(false)
-    const closeMenu = () => setClick(false)
 
     return (
-        <nav className= 'navbar'>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-                <li>
-                    <Link to= "Home" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Home</Link>
+        <nav className= 'bg-white dark:bg-gray-950 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600'>
+            <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+                <h2 className='text-white'>Piero Rodriguez</h2>
+            <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+            <ul className= "flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li className='cursor-pointer'>
+                    <Link className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500' aria-current="page" to= "Home" spy={true} smooth={true} offset={-100} duration={500} >Home</Link>
                 </li>
-                <li>
-                    <Link to= "About" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>About Me</Link>
+                <li className='cursor-pointer'>
+                    <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "About" spy={true} smooth={true} offset={-100} duration={500} >About Me</Link>
                 </li>
-                <li>
-                    <Link to= "Projects" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Projects</Link>
+                <li className='cursor-pointer'>
+                    <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "Projects" spy={true} smooth={true} offset={-100} duration={500}>Projects</Link>
                 </li>
-                <li>
-                    <Link to= "Resume" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Resume</Link>
+                <li className='cursor-pointer'>
+                    <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "Resume" spy={true} smooth={true} offset={-100} duration={500} >Resume</Link>
                 </li>
-                <li>
-                    <Link to= "Contact" spy={true} smooth={true} offset={-150} duration={500} onClick={closeMenu}>Contact Me</Link>
+                <li  className='cursor-pointer'>
+                    <Link className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "Contact" spy={true} smooth={true} offset={-150} duration={500}>Contact Me</Link>
                 </li>
             </ul>
+            </div>
+            </div>
         </nav>
     )
 }
 
 
 export default NavTabs
+
