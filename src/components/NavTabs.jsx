e01530af743860c5399f24bb33c9ed7c27534fc0
Piero Rodriguez <piero@Pieros-MacBook-Pro.local>
2023-12-14 02:10:36 -0500

    gave navbar color effect when user is actively on the section

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -54,10 +54,9 @@ const NavTabs = () => {
               isMenuOpen ? 'block' : 'hidden'
             } mt-5 mr-5 font-bold space-y-2 md:space-y-0 md:space-x-8 md:flex md:w-auto md:absolute md:top-0 md:right-0`}
           >
-            <li className="cursor-pointer">
+            <li className="nav-item cursor-pointer">
               <Link
-                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                aria-current="page"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 to="home"
                 spy={true}
                 smooth={true}
@@ -68,16 +67,16 @@ const NavTabs = () => {
                 Home
               </Link>
             </li>
-            <li className='cursor-pointer'>
+            <li className='nav-item cursor-pointer'>
                 <Link onClick={() => setIsMenuOpen(false)} className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "about" spy={true} smooth={true} offset={-100} duration={500} >About Me</Link>
             </li>
-            <li className='cursor-pointer'>
+            <li className='nav-item cursor-pointer'>
                 <Link onClick={() => setIsMenuOpen(false)} className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "projects" spy={true} smooth={true} offset={-100} duration={500}>Projects</Link>
             </li>
-            <li className='cursor-pointer'>
+            <li className='nav-item cursor-pointer'>
                 <Link onClick={() => setIsMenuOpen(false)} className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "resume" spy={true} smooth={true} offset={-100} duration={500} >Resume</Link>
             </li>
-            <li  className='cursor-pointer'>
+            <li  className='nav-item cursor-pointer'>
                 <Link onClick={() => setIsMenuOpen(false)} className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700' to= "contact" spy={true} smooth={true} offset={-150} duration={500}>Contact Me</Link>
             </li>
             </ul>
